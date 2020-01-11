970a5bfa63cf402afa5822659118fce423d51e95
Yasir Yacob <alyasiryacob@gmail.com>
2020-01-11 22:32:25 +0800

    Successfully connected to database using MySQL

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var express = require('express')
 var app = express()
 const port = 3000
+var mysql = require('mysql')
+
+var myConnection  = require('express-myconnection')
+
+var config = require('./config/db')
+var dbOptions = {
+    host:      config.database.host,
+    user:       config.database.user,
+    password: config.database.password,
+    port:       config.database.port, 
+    database: config.database.db
+}
+
+app.use(myConnection(mysql, dbOptions, 'pool'))
+
+const conn = mysql.createConnection(dbOptions)
 
 var bodyParser = require('body-parser')
 
@@ -13,4 +29,15 @@ var index = require('./routes/index')
 app.use(bodyParser.json());
 
 app.use('/', index)
+
 app.listen(port, () => console.log(`Server running at port ${port}!`))
+
+//connect to database
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('MySQL Connected...');
+});
+
+
+
+  
---
+++ config/db.js
@@ -0,0 +1,10 @@
+var config = {
+	database: {
+		host:	  'localhost', 	// database host
+		user: 	  'root', 		// your database username
+		password: '', 		// your database password
+		db: 	  'inventory' 		// your database name
+	},
+	
+}
+module.exports = config
